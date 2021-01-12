1d564ee6e08862732d8bd2eca37f858b6d0999c8
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-01-12 10:04:34 -0300

    Add CRUD for Turma, Niveis and Matriculas tables

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,10 +3,16 @@ const PessoaController = require('../controllers/PessoaController');
 
 const router = Router();
 
-router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
-router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
-router.post('/pessoas', PessoaController.criaPessoa);
-router.put('/pessoas/:id', PessoaController.atualizaPessoa);
-router.delete('/pessoas/:id', PessoaController.apagaPessoa);
+router
+  .get('/pessoas', PessoaController.pegaTodasAsPessoas)
+  .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+  .post('/pessoas', PessoaController.criaPessoa)
+  .put('/pessoas/:id', PessoaController.atualizaPessoa)
+  .delete('/pessoas/:id', PessoaController.apagaPessoa)
+  .get('/pessoas/:estudanteId/matricula', PessoaController.pegaTodasAsMatriculasdeUmaPessoa)
+  .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+  .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+  .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+  .delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
 
 module.exports = router;
