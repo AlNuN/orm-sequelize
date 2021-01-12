653af230677acbda6e005214bd1fe4e26026f449
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-01-12 17:21:09 -0300

    Return classes that are full

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -16,6 +16,8 @@ router
   .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
   .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
   .delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
-  .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula);
+  .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
+  .get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculasPorTurma)
+  .get('/pessoas/matricula/lotada', PessoaController.pegaTurmasLotadas);
 
 module.exports = router;
